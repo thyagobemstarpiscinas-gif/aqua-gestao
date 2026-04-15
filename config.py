9f9aa82d709e660a6b7743c39e775ff7f7f1b47a
Thyago <thyago.bemstarpiscinas@gmail.com>
2026-04-14 21:23:22 -0300

    Atualiza app e arquivos de suporte

---
+++ config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+BASE_DIR = Path.cwd()
+TEMPLATES_DIR = BASE_DIR / "templates"
+GENERATED_DIR = BASE_DIR / "generated"
+
+EMPRESA_NOME = "Aqua Gestão – Controle Técnico de Piscinas"
+RESPONSAVEL_TECNICO = "Thyago Silveira"
+CRQ = "024025748"
